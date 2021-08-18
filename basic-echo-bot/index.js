a682e00f75d83343ef1e5e4616ef543c85b42262
Nir Fainshtein <fainir2006@gmail.com>
2021-08-18 10:50:50 +0300

    improved the code

--- basic-echo-bot/index.js
+++ basic-echo-bot/index.js
@@ -2,81 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const dialogflow = require('@google-cloud/dialogflow');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const TOKEN = 'YOUR_TOKEN'
-const DIALOGFLOW_PROJECT_ID = 'YOUR_DIALOGFLOW_PROJECT_ID'
-const TITLE = 'BOT_NAME'
-const API_URL = 'https://unissist.ue.r.appspot.com'
-const MY_DOMAIN = 'YOUR_SERVER_DOMAIN'
-const MY_ENDPOINT = `${MY_DOMAIN}/messages/${TOKEN}`
+const TOKEN = 'YOUR_TOKEN';
+const TITLE = 'BOT_NAME';
+const MY_DOMAIN = 'YOUR_SERVER_DOMAIN';
+const MY_ENDPOINT = `${MY_DOMAIN}/messages/${TOKEN}`;
+const API_URL = 'https://unissist.ue.r.appspot.com';
 
 app.get(`/init`, async (req, res) => {
     let response = await setup();
     res.send(response.data);
-})
+});
 
 app.post(`/messages/${TOKEN}`, async (req, res) => {
-    const dialogResponse = await callDialogFlow(req.body.text, req.body.sessionId);
-    res.send(dialogResponse)
+    res.send(`The text is ${req.body.text} and the sessionId is ${req.body.sessionId}`);
 });
 
 const setup = async () => {
   try {        
-      const res = await axios.get(`${API_URL}/messages/init?token=${TOKEN}&title=${TITLE}&endpoint=${MY_ENDPOINT}`)
-      console.log(res.data)    
+      const res = await axios.get(`${API_URL}/messages/init?token=${TOKEN}&title=${TITLE}&endpoint=${MY_ENDPOINT}`);
       return res;
   } catch (err) {
       console.log(err);
-  }
-}
-
-async function callDialogFlow(text, sessionId) {
-    // A unique identifier for the given session
-    const projectId = DIALOGFLOW_PROJECT_ID;
-  
-    // Create a new session
-    const sessionClient = new dialogflow.SessionsClient({keyFilename: 'dialogflowconfig.json'});
-    const sessionPath = sessionClient.projectAgentSessionPath(
-      projectId,
-      sessionId
-    );
-  
-    // The text query request.
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          // The query to send to the dialogflow agent
-          text: text,
-          // The language used by the client (en-US)
-          languageCode: 'en-US',
-        },
-      },
-    };
-  
-    // Send request and log result
-    try{
-      const responses = await sessionClient.detectIntent(request);
-      console.log('Detected intent');
-      const result = responses[0].queryResult;
-      console.log(`Query: ${result.queryText}`);
-      console.log(`Response: ${result.fulfillmentText}`);
-      if (result.intent) {
-        console.log(`Intent: ${result.intent.displayName}`);
-      } else {
-        console.log('No intent matched.');
-      }
-      return result.fulfillmentText;
-    }
-
-catch(err){
-    console.log(err);
-    return '';
   }
 }
 
--- smalltalk-using-dialogflow-bot/index.js
+++ smalltalk-using-dialogflow-bot/index.js
@@ -2,35 +2,73 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const dialogflow = require('@google-cloud/dialogflow');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const TOKEN = 'YOUR_TOKEN'
-const DIALOGFLOW_PROJECT_ID = 'YOUR_DIALOGFLOW_PROJECT_ID'
-const TITLE = 'BOT_NAME'
+const TOKEN = 'YOUR_TOKEN';
+const DIALOGFLOW_PROJECT_ID = 'YOUR_DIALOGFLOW_PROJECT_ID';
+const TITLE = 'BOT_NAME';
+const MY_DOMAIN = 'YOUR_SERVER_DOMAIN';
+const MY_ENDPOINT = `${MY_DOMAIN}/messages/${TOKEN};`
+
 const API_URL = 'https://unissist.ue.r.appspot.com'
-const MY_DOMAIN = 'YOUR_SERVER_DOMAIN'
-const MY_ENDPOINT = `${MY_DOMAIN}/messages/${TOKEN}`
 
 app.get(`/init`, async (req, res) => {
     let response = await setup();
     res.send(response.data);
-})
+});
 
 app.post(`/messages/${TOKEN}`, async (req, res) => {
-    res.send(`The text is ${req.body.text} and the sessionId is ${req.body.sessionId}`)
+    const dialogResponse = await callDialogFlow(req.body.text, req.body.sessionId);
+    res.send(dialogResponse);
 });
 
 const setup = async () => {
   try {        
-      const res = await axios.get(`${API_URL}/messages/init?token=${TOKEN}&title=${TITLE}&endpoint=${MY_ENDPOINT}`)
-      console.log(res.data)    
+      const res = await axios.get(`${API_URL}/messages/init?token=${TOKEN}&title=${TITLE}&endpoint=${MY_ENDPOINT}`);
       return res;
   } catch (err) {
       console.log(err);
   }
+}
+
+async function callDialogFlow(text, sessionId) {
+    // A unique identifier for the given session
+    const projectId = DIALOGFLOW_PROJECT_ID;
+  
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient({keyFilename: 'dialogflowconfig.json'});
+    const sessionPath = sessionClient.projectAgentSessionPath(
+      projectId,
+      sessionId
+    );
+  
+    // The text query request.
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          // The query to send to the dialogflow agent
+          text: text,
+          // The language used by the client (en-US)
+          languageCode: 'en-US',
+        },
+      },
+    };
+  
+    // Send request and log result
+    try{
+      const responses = await sessionClient.detectIntent(request);
+      const result = responses[0].queryResult;
+      return result.fulfillmentText;
+    }
+    catch(err){
+        console.log(err);
+        return '';
+    }
 }
 
 const PORT = process.env.PORT || 8080;
